bf83edb8a4a195fb6e7e6c8dfb9111e5ec20c787
Maksimchik36 <92870549+Maksimchik36@users.noreply.github.com>
2022-05-24 14:29:27 +0000

    Deploying to gh-pages from @ Maksimchik36/goit-js-hw-11@1636f6fb556057fc874432b674181aabe9712c6b 🚀

--- src.42b63341.js
+++ src.42b63341.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/search.jpg":[["search.26655cb1.jpg","a2dl"],"a2dl"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11/src.c82325fa.js.map
+//# sourceMappingURL=/goit-js-hw-11/src.42b63341.js.map
