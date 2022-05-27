693fb4f5ab933b50c3bc13368b8c68c718200769
Maksimchik36 <Peltek1985@gmail.com>
2022-05-27 17:31:07 +0300

    add functions

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ formRef.addEventListener("submit", onSearch);
 
 function onSearch(event) {
     event.preventDefault();
-    console.log(event.currentTarget.elements.searchQuery.value);
-    fetchImages(event.currentTarget.elements.searchQuery.value);
+    // console.log(event.currentTarget.elements.searchQuery.value);
+    console.log(fetchImages(event.currentTarget.elements.searchQuery.value));
 }
 
 
@@ -93,4 +93,57 @@ function onSearch(event) {
 //      const markup = renderGallery(images);
 //     galleryEl.insertAdjacentHTML('beforeend', markup);
 //   });
+// }
+
+
+
+
+
+       //    FROM HOMEWORK 10
+
+//        import './css/styles.css';
+// import countryCardTpl from './templates/country-card.hbs';
+// import countryCardsTpl from './templates/list-country-cards.hbs';
+
+// import Notiflix from 'notiflix';
+// import {fetchCountries} from "./js/fetchCountries";
+// import debounce from "lodash.debounce";
+// const DEBOUNCE_DELAY = 300;
+
+// const inputRef = document.querySelector("input#search-box");
+// const countryInfoRef = document.querySelector(".country-info"); 
+// const countriesListRef = document.querySelector(".country-list");
+
+// inputRef.addEventListener("input", debounce(renderList, DEBOUNCE_DELAY));
+
+// function renderList(event){
+//     const userRequest = event.target.value.trim();
+    
+//     if (userRequest === '') {
+//         countryInfoRef.innerHTML = "";
+//         countriesListRef.innerHTML ="";
+//         return;
+//     }
+ 
+//     fetchCountries(userRequest)
+//         .then(array => {
+//             if (array.length >10) {
+//                 countryInfoRef.innerHTML = "";
+//                 countriesListRef.innerHTML ="";
+//                 Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+//             } else if (array.length >= 2) {
+//                 countryInfoRef.innerHTML = "";                
+//                 const markup = countryCardsTpl(array);
+//                 countriesListRef.innerHTML = markup;
+//             } else {
+//                 countriesListRef.innerHTML ="";                
+//                 const markup = countryCardTpl(array);
+//                 countryInfoRef.innerHTML = markup;
+//             }
+//     })
+//         .catch(error => {
+//             countryInfoRef.innerHTML = "";
+//             countriesListRef.innerHTML ="";
+//             Notiflix.Notify.failure("Oops, there is no country with that name.")
+//         });
 // }
