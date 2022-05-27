de03b239cfb6c6b53e05d87e113b394c0aed0557
Maksimchik36 <Peltek1985@gmail.com>
2022-05-27 17:12:29 +0300

    add code from pagination test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,96 @@
 import './sass/main.scss';
+import { fetchImages } from './js/fetchImages';
+
+const formRef = document.querySelector("#search-form");
+
+formRef.addEventListener("submit", onSearch);
+
+function onSearch(event) {
+    event.preventDefault();
+    console.log(event.currentTarget.elements.searchQuery.value);
+    fetchImages(event.currentTarget.elements.searchQuery.value);
+}
+
+
+
+
+
+
+
+
+
+           // FROM GINATION-TEST index.js
+
+
+// import './sass/main.scss';
+// import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
+// import apiService from "./js/api";
+// import renderGallery from './js/render_images'
+
+// const galleryEl = document.querySelector('.gallery');
+// const formEl = document.querySelector('.search-form');
+
+// let searchQuery = '';
+
+// const options = {
+//   totalItems: 0,
+//   itemsPerPage: 3,
+//   visiblePages: 10,
+//   page: 1,
+// };
+
+
+// apiService.itemsPerPage = options.itemsPerPage;
+
+
+
+// const pagination = new Pagination('pagination', options);
+// // console.dir(pagination);
+
+// const page = pagination.getCurrentPage();
+
+
+// apiService.fetchPopularImages(page).then(({ images, total }) => {
+//    const markup = renderGallery(images);
+//     galleryEl.insertAdjacentHTML('beforeend', markup);
+//   pagination.reset(total);
+// });
+
+// function popular(event) {
+//   const currentPage = event.page;
+//   apiService.fetchPopularImages(currentPage).then(({ images, total }) => {
+//     const markup = renderGallery(images);
+//     galleryEl.insertAdjacentHTML('beforeend', markup);
+//   });
+// }
+
+// pagination.on('afterMove', popular);
+
+// formEl.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   galleryEl.innerHTML = '';
+
+//   pagination.off('afterMove', popular);
+//   pagination.movePageTo(1);
+//   pagination.on('afterMove', bySearch);
+
+//   apiService.searchQuery = event.currentTarget.elements.searchQuery.value;
+  
+//   apiService.fetchImagesByName(page).then(({ images, total }) => {
+//    const markup = renderGallery(images);
+//     galleryEl.insertAdjacentHTML('beforeend', markup);
+//     pagination.reset(total);
+//   });
+// }
+
+// function bySearch(event) {
+//   const currentPage = event.page;
+//   apiService.fetchImagesByName(currentPage).then(({ images }) => {
+//      const markup = renderGallery(images);
+//     galleryEl.insertAdjacentHTML('beforeend', markup);
+//   });
+// }
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -6,10 +6,81 @@ const KEY = "27623768-405768f09194e046df4a054c4";
 // https://pixabay.com/api/?key=27623768-405768f09194e046df4a054c4&q=yellow+flowers&image_type=photo&pretty=true
 
 export function fetchImages(name){
-    return fetch(`${BASE_URL}?key=${KEY}&q=${input.value}&image_type=photo&orientation="horizontal"&safesearch="true"`).then(response => {
+    return fetch(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation="horizontal"&safesearch="true"`).then(response => {
         if (!response.ok){
             throw new Error('Error fetching data');
         }
         return response.json();
     })   
 }
+
+
+
+          // FROM GINATION-TEST api.js
+
+
+// const BASE_URL = 'https://pixabay.com/api/';
+// const KEY = '23788919-1e868a4f1ae72234cc449d190';
+
+
+// export default {
+
+//  itemsPerPage: 0,
+//   searchQuery: '',
+    
+//   // create object hits, totalhits, total
+// fetchPopularImages(page) {
+//   console.log(page);
+//   return fetch(
+//     `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&page=${page}&per_page=${this.itemsPerPage}`,
+//   )
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .then(data => ({ images: data.hits, total: data.totalHits }));
+// },
+
+
+// fetchImagesByName(page) {
+
+//   return fetch(
+//     `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&page=${page}&per_page=${this.itemsPerPage}`,
+//   )
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .then(data => ({ images: data.hits, total: data.totalHits }));
+//   }
+// }
+
+
+
+
+//  from render-images.js
+
+// // Рисует текстовую разметку для картинок
+// export default function renderGallery(images) {
+//   return images
+//     .map(image => {
+//       const { largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
+//       return `<a class="gallery__link" href="${largeImageURL}">
+//           <div class="gallery-item">
+//             <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" width="250"/>
+//             <div class="info">
+//               <p class="info-item"><b>Likes</b>${likes}</p>
+//               <p class="info-item"><b>Views</b>${views}</p>
+//               <p class="info-item"><b>Comments</b>${comments}</p>
+//               <p class="info-item"><b>Downloads</b>${downloads}</p>
+//             </div>
+//           </div>
+//         </a>`;
+//     })
+//     .join('');
+  
+// }
