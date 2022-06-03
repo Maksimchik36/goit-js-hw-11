6d9a710ca1e146d6d1ebba986133bf1c6855d771
Maksimchik36 <Peltek1985@gmail.com>
2022-06-03 19:12:09 +0300

    add axios and async/await

--- src/index.js
+++ src/index.js
@@ -12,11 +12,10 @@ loadMoreRef.addEventListener("click", onBtnLoadMoreClick)
 
 let searchQuery = '';
 
-function onFormSubmit(event) {
+async function onFormSubmit(event) {
     event.preventDefault();
     galleryRef.innerHTML = "";
-    // let page = 1;
-
+    
     searchQuery = event.currentTarget.elements.searchQuery.value.trim();
 
     if (searchQuery === '') {            
@@ -27,17 +26,15 @@ function onFormSubmit(event) {
     }
             apiService.page = 1;
 
-    apiService.searchImages(searchQuery).then(data => { renderImages(data.hits); return data; })
-        .then(data => {
-        if (data.totalHits > 0) {
-            Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    const result = await apiService.searchImages(searchQuery);
+    renderImages(result.data.hits);
+        
+        if (result.data.totalHits > 0) {
+            Notiflix.Notify.success(`Hooray! We found ${result.data.totalHits} images.`);
             loadMoreRef.classList.add('load-more-visible');
         } else {
             Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-             }
-        });
-    
-    
+             }   
 }
 
 function renderImages(hits){
@@ -45,147 +42,14 @@ function renderImages(hits){
     galleryRef.insertAdjacentHTML("beforeend", markup);
 }
 
-// function renderList(event){
-//         
-//         
-     
-//         return apiService.searchImages(userRequest)
-//             .then(array => {
-//                 if(array.total === 0){
-//                     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-//                 }               
-//         })
-//             .catch(error => {
-//                 console.log("error.message", error.message);
-//             });
-//     }
-
-//   
-
-    function onBtnLoadMoreClick(){
+async function onBtnLoadMoreClick(){
         apiService.page += 1;
 
-        apiService.searchImages(searchQuery).then(data => { renderImages(data.hits); return data }).then(data => {
-            if (data.hits.length < apiService.itemsPerPage) {
-                Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
-            loadMoreRef.classList.remove('load-more-visible');
-        }
-        }
-
-        )
-    }
-
-
-
-
-
-
-
-
-           // FROM GINATION-TEST index.js
-
-
-// import './sass/main.scss';
-
-// import apiService from "./js/api";
-// import renderGallery from './js/render_images'
-
-// const galleryEl = document.querySelector('.gallery');
-// const formEl = document.querySelector('.search-form');
-
-// let searchQuery = '';
-
-
-
-// apiService.fetchPopularImages(page).then(({ images, total }) => {
-//    const markup = renderGallery(images);
-//     galleryEl.insertAdjacentHTML('beforeend', markup);
-//
-// });
-
-// function popular(event) {
-//   const currentPage = event.page;
-//   apiService.fetchPopularImages(currentPage).then(({ images, total }) => {
-//     const markup = renderGallery(images);
-//     galleryEl.insertAdjacentHTML('beforeend', markup);
-//   });
-// }
-
-
-// formEl.addEventListener('submit', onFormSubmit);
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-
-//   galleryEl.innerHTML = '';
-
-//  
-
-//   apiService.searchQuery = event.currentTarget.elements.searchQuery.value;
-  
-//   apiService.fetchImagesByName(page).then(({ images, total }) => {
-//    const markup = renderGallery(images);
-//     galleryEl.insertAdjacentHTML('beforeend', markup);
-//   });
-// }
-
-// function bySearch(event) {
-//   const currentPage = event.page;
-//   apiService.fetchImagesByName(currentPage).then(({ images }) => {
-//      const markup = renderGallery(images);
-//     galleryEl.insertAdjacentHTML('beforeend', markup);
-//   });
-// }
-
-
-
-
-
-       //    FROM HOMEWORK 10
-
-//        import './css/styles.css';
-// import countryCardTpl from './templates/country-card.hbs';
-// import countryCardsTpl from './templates/list-country-cards.hbs';
-
-// import Notiflix from 'notiflix';
-// import {fetchCountries} from "./js/fetchCountries";
-// import debounce from "lodash.debounce";
-// const DEBOUNCE_DELAY = 300;
-
-// const inputRef = document.querySelector("input#search-box");
-// const countryInfoRef = document.querySelector(".country-info"); 
-// const countriesListRef = document.querySelector(".country-list");
-
-// inputRef.addEventListener("input", debounce(renderList, DEBOUNCE_DELAY));
-
-// function renderList(event){
-//     const userRequest = event.target.value.trim();
+const result = await apiService.searchImages(searchQuery);
+    renderImages(result.data.hits);
     
-//     if (userRequest === '') {
-//         countryInfoRef.innerHTML = "";
-//         countriesListRef.innerHTML ="";
-//         return;
-//     }
- 
-//     fetchCountries(userRequest)
-//         .then(array => {
-//             if (array.length >10) {
-//                 countryInfoRef.innerHTML = "";
-//                 countriesListRef.innerHTML ="";
-//                 Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-//             } else if (array.length >= 2) {
-//                 countryInfoRef.innerHTML = "";                
-//                 const markup = countryCardsTpl(array);
-//                 countriesListRef.innerHTML = markup;
-//             } else {
-//                 countriesListRef.innerHTML ="";                
-//                 const markup = countryCardTpl(array);
-//                 countryInfoRef.innerHTML = markup;
-//             }
-//     })
-//         .catch(error => {
-//             countryInfoRef.innerHTML = "";
-//             countriesListRef.innerHTML ="";
-//             Notiflix.Notify.failure("Oops, there is no country with that name.")
-//         });
-// }
+    if (result.data.hits.length < apiService.itemsPerPage) {
+        Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+        loadMoreRef.classList.remove('load-more-visible');
+    }
+}
--- src/js/api.js
+++ src/js/api.js
@@ -1,3 +1,5 @@
+const axios = require('axios').default;
+
 const BASE_URL = "https://pixabay.com/api/";
 const KEY = "27623768-405768f09194e046df4a054c4";
 
@@ -6,81 +8,25 @@ export default {
     itemsPerPage: 5,
     page: 1,
         
-    searchImages(searchQuery){
-    return fetch(`${BASE_URL}?key=${KEY}&q=${searchQuery}&page=${this.page}&per_page=${this.itemsPerPage}&image_type=photo&orientation=horizontal&safesearch=true`).then(response => {
-        if (!response.ok){
-            throw new Error('Error fetching data');
-        }
-        // console.log(response.data);
-        return response.json();
-    })   
-}
-}
-
-
-
-
-          // FROM PAGINATION-TEST api.js
-
-
-// export default {
-
-//  itemsPerPage: 0,
-//   searchQuery: '',
+//     searchImages(searchQuery){
+//     return fetch(`${BASE_URL}?key=${KEY}&q=${searchQuery}&page=${this.page}&per_page=${this.itemsPerPage}&image_type=photo&orientation=horizontal&safesearch=true`).then(response => {
+//         if (!response.ok){
+//             throw new Error('Error fetching data');
+//         }
+//         // console.log(response.data);
+//         return response.json();
+//     })
+// }
     
-//   // create object hits, totalhits, total
-// fetchPopularImages(page) {
-//   console.log(page);
-//   return fetch(
-//     `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&page=${page}&per_page=${this.itemsPerPage}`,
-//   )
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     })
-//     .then(data => ({ images: data.hits, total: data.totalHits }));
-// },
+    async searchImages(searchQuery) {
+  try {
+      const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${searchQuery}&page=${this.page}&per_page=${this.itemsPerPage}&image_type=photo&orientation=horizontal&safesearch=true`);
+    //   console.log(response);
+      return response;
+  } catch (error) {
+    //   console.error(error);
+      throw new Error('Error fetching data');
+  }
+}
+}
 
-
-// fetchImagesByName(page) {
-
-//   return fetch(
-//     `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&page=${page}&per_page=${this.itemsPerPage}`,
-//   )
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     })
-//     .then(data => ({ images: data.hits, total: data.totalHits }));
-//   }
-// }
-
-
-
-
-//  from render-images.js
-
-// // Рисует текстовую разметку для картинок
-// export default function renderGallery(images) {
-//   return images
-//     .map(image => {
-//       const { largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
-//       return `<a class="gallery__link" href="${largeImageURL}">
-//           <div class="gallery-item">
-//             <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" width="250"/>
-//             <div class="info">
-//               <p class="info-item"><b>Likes</b>${likes}</p>
-//               <p class="info-item"><b>Views</b>${views}</p>
-//               <p class="info-item"><b>Comments</b>${comments}</p>
-//               <p class="info-item"><b>Downloads</b>${downloads}</p>
-//             </div>
-//           </div>
-//         </a>`;
-//     })
-//     .join('');
-  
-// }
